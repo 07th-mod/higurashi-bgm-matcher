c311f04e65be3596f4144ba47374e9435935c8f8
Daniel Wong <drojfjord@gmail.com>
2022-10-22 20:33:40 +1100

    Load and query databases separately

--- EasyFingerPrinting.cs
+++ EasyFingerPrinting.cs
@@ -3,39 +3,54 @@ using SoundFingerprinting.Audio;
 using SoundFingerprinting.Builder;
 using SoundFingerprinting.Data;
 using SoundFingerprinting.InMemory;
+using SoundFingerprinting.Query;
 
 namespace Info
 {
-
     class EasyFingerPrinting
     {
         InMemoryModelService modelService;
         IAudioService audioService;
+        string directoryToSearch;
         string deserPath;
+        bool rebuild;
+        int? maxToProcess;
 
-        public EasyFingerPrinting(string deserPath)
+        public EasyFingerPrinting(string directoryToSearch, string deserPath, bool rebuild = false, int? maxToProcess = null)
         {
+            this.rebuild = rebuild;
+            this.directoryToSearch = directoryToSearch;
             this.deserPath = deserPath;
             this.modelService = new InMemoryModelService(); // store fingerprints in RAM
             this.audioService = new SoundFingerprintingAudioService(); // default audio library
+            this.maxToProcess = maxToProcess;
         }
 
-        public async Task LoadOrRegenerate(string[] database_paths, Dictionary<string, VideoInfo> pathToVideoInfo, bool rebuild)
+        public async Task LoadOrRegenerate()
         {
             if (rebuild || !Directory.Exists(deserPath))
             {
+                string[] database_paths = Directory.GetFiles(directoryToSearch, "*.*", SearchOption.AllDirectories);
+
+                int i = 0;
                 foreach (string path in database_paths)
                 {
+                    i++;
                     // VideoInfo info = new VideoInfo(path);
-                    // Console.WriteLine(path);
+                    Console.WriteLine($"Processing {i}/{database_paths.Count()}: {path}");
                     // sb.AppendLine(info.ToString());
-                    Console.WriteLine(pathToVideoInfo[path]);
+                    // Console.WriteLine(pathToVideoInfo[path]);
                     await StoreForLaterRetrieval(path);
+
+                    if (maxToProcess != null && i >= maxToProcess)
+                    {
+                        break;
+                    }
                 }
 
                 // File.WriteAllText("testunicode.txt", sb.ToString());
 
-                modelService.Snapshot("snapshot");
+                modelService.Snapshot(deserPath);
             }
             else
             {
@@ -63,51 +78,18 @@ namespace Info
             modelService.Insert(track, avHashes);
         }
 
-        public async Task QueryPath(Dictionary<string, VideoInfo> pathToVideoInfo, StringBuilder sb, string path)
+        public async Task<AVQueryResult> QueryPath(Dictionary<string, VideoInfo> pathToVideoInfo, StringBuilder sb, string path)
         {
             int secondsToAnalyse = 10;
             int startAtSecond = 0;
 
-            var queryResult = await QueryCommandBuilder.Instance
+            return await QueryCommandBuilder.Instance
                                             .BuildQueryCommand()
                                             .From(path)
                                             // .From(path, secondsToAnalyse, startAtSecond)
                                             .UsingServices(modelService)
                                             .Query();
-
-
-            // Console.WriteLine($"Best Match: {queryResult.BestMatch.TrackId}");
-
-            sb.Append($"\"{path}\",");
-
-            if (queryResult.BestMatch != null)
-            {
-                VideoInfo info = pathToVideoInfo[queryResult.BestMatch.TrackId];
-                //{entry.TrackCoverageWithPermittedGapsLength:0.00}/{entry.Track.Length},
-                sb.Append($"\"{info.name}\",\"{info.source}\",\"{info.url}\",");
-            }
-
-
-            // // iterate over the results if any
-            // foreach(var (entry, _) in queryResult.ResultEntries)
-            // {
-            //     // output only those tracks that matched at least seconds.
-            //     // if(path != entry.Track.Id && entry.TrackCoverageWithPermittedGapsLength >= 5d)
-            //     // {
-            //         VideoInfo info = pathToVideoInfo[entry.Track.Id];
-            //         //{entry.TrackCoverageWithPermittedGapsLength:0.00}/{entry.Track.Length},
-            //         sb.Append($"\"{info.name}\",\"{info.source}\",\"{info.url}\",");
-            //     // }
-            // }
-
-            sb.AppendLine();
         }
-
-        void Serialize()
-        {
-            modelService.Snapshot(deserPath);
-        }
-
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,57 +1,76 @@
 ﻿using Info;
-using SoundFingerprinting;
-using SoundFingerprinting.Audio;
-using SoundFingerprinting.Builder;
-using SoundFingerprinting.Data;
-using SoundFingerprinting.InMemory;
 using System.Text;
-using System.Text.RegularExpressions;
 
-// string snapshotFolder = "snapshot";
+// This folder contains named music files which are used to build the database
+string reference_folder = "reference";
+bool force_rebuild = false;
+int? maxToProcess = null;
 
-EasyFingerPrinting fp = new EasyFingerPrinting("snapshot");
+EasyFingerPrinting[] fingerPrinterCascade = new EasyFingerPrinting[] {
+    new EasyFingerPrinting(Path.Combine(reference_folder, "mario"), "database_mario", force_rebuild, maxToProcess),
+    new EasyFingerPrinting(Path.Combine(reference_folder, "manual_matches"), "database_manual_matches", force_rebuild, maxToProcess),
+    new EasyFingerPrinting(Path.Combine(reference_folder, "console_hou"), "database_console_hou", force_rebuild, maxToProcess),
+};
 
-// TODO: split into different databases, so can prefer the Youtube database first?
-
-string database_folder = "reference";
-string query_folder = "mod";
-string youtube_folder = "mario";
-
-string[] database_paths = Directory.GetFiles(database_folder, "*.*", SearchOption.AllDirectories);
-string[] query_paths = Directory.GetFiles(query_folder, "*.*", SearchOption.AllDirectories);
-
-bool rebuild = false;
-
-Console.WriteLine($"--------------- Loading/Generating Database [{database_folder}]-----------------");
-
+Console.WriteLine($"--------------- Building Filename List [{reference_folder}]-----------------");
 Dictionary<string, VideoInfo> pathToVideoInfo = new Dictionary<string, VideoInfo>();
-
-foreach (string path in database_paths)
 {
-    VideoInfo info = new VideoInfo(path, path.Contains(youtube_folder) ? Mode.YOUTUBE_DL : Mode.CONSOLE);
-    // Console.WriteLine($"Parsing filename {info}");
-    pathToVideoInfo[path] = info;
+    string youtube_folder = "mario";
+    string[] database_paths = Directory.GetFiles(reference_folder, "*.*", SearchOption.AllDirectories);
+    foreach (string path in database_paths)
+    {
+        VideoInfo info = new VideoInfo(path, path.Contains(youtube_folder) ? Mode.YOUTUBE_DL : Mode.CONSOLE);
+        // Console.WriteLine($"Parsing filename {info}");
+        pathToVideoInfo[path] = info;
+    }
 }
 
-await fp.LoadOrRegenerate(database_paths, pathToVideoInfo, rebuild: false);
+Console.WriteLine($"--------------- Loading/Generating Database-----------------");
+foreach (var fingerPrinter in fingerPrinterCascade)
+{
+    await fingerPrinter.LoadOrRegenerate();
+}
 
+
+string query_folder = "mod";
 Console.WriteLine($"--------------- Begin querying [{query_folder}]-----------------");
+string[] query_paths = Directory.GetFiles(query_folder, "*.*", SearchOption.AllDirectories); ;
 
 StringBuilder sb = new StringBuilder();
 
 sb.AppendLine("path, name1, source1, url1, name2, source2, url2, name3, source3, url3,");
 
-int cnt = 1;
+int cnt = 0;
 foreach (string path in query_paths)
 {
-    // if(!path.Contains("hm01_01"))
-    // {
-    //     continue;
-    // }
-
-    Console.WriteLine($"Processing [{cnt}/{query_paths.Length}]: {path}");
-    await fp.QueryPath(pathToVideoInfo, sb, path);
     cnt++;
+    Console.WriteLine($"Processing [{cnt}/{query_paths.Length}]: {path}");
+
+    // Search through each database looking for a match
+    VideoInfo? match = null;
+    foreach (var fingerPrinter in fingerPrinterCascade)
+    {
+        var queryResult = await fingerPrinter.QueryPath(pathToVideoInfo, sb, path);
+        // Console.WriteLine($"Best Match: {queryResult.BestMatch.TrackId}");
+        if (queryResult.BestMatch != null)
+        {
+            //{entry.TrackCoverageWithPermittedGapsLength:0.00}/{entry.Track.Length},
+            match = pathToVideoInfo[queryResult.BestMatch.TrackId];
+            break;
+        }
+    }
+
+    sb.Append($"\"{path}\",");
+    if (match != null)
+    {
+        sb.Append($"\"{match.name}\",\"{match.source}\",\"{match.url}\",");
+    }
+    sb.AppendLine();
+
+    if (cnt >= maxToProcess)
+    {
+        break;
+    }
 }
 
 File.WriteAllText("query_result.csv", sb.ToString());
